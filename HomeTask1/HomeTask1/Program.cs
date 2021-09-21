e53a54346663d50066713907df8d5fb75879d532
Rinat.Muginov <Rinat.Muginov@urfu.me>
2021-09-21 18:15:04 +0500

    выполнено задание 1

---
+++ HomeTask1/HomeTask1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("В.В. Маяковский");
+			Console.WriteLine("А вы могли бы");
+			Console.WriteLine(" ");
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("Я сразу смазал карту будня,");
+			Console.WriteLine("плеснувши краску из стакана;");
+			Console.WriteLine("я показал на блюде студня");
+			Console.WriteLine("косые скулы океана.");
+			Console.WriteLine("На чешуе жестяной рыбы");
+			Console.WriteLine("прочел я зовы новых губ.");
+			Console.WriteLine("А вы");
+			Console.WriteLine("ноктюрн сыграть");
+			Console.WriteLine("могли бы");
+			Console.WriteLine("на флейте водосточных труб?");
+			Console.ForegroundColor = ConsoleColor.Gray;
+			Console.ReadKey();
+
+		}
+	}
+}
