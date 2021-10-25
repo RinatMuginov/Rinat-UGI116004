3d31da1766c59122e5f0189f661b6f754025dd13
Rinat.Muginov <Rinat.Muginov@urfu.me>
2021-10-25 23:35:45 +0500

    домашние задание 3

---
+++ HomeTask3/HomeTask3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeTask3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите значения аргумента для функции f(x)");
+            var argument = double.Parse(Console.ReadLine());
+            if ((argument == 0) || (argument < 1))
+                Console.WriteLine("Значение аргумента не удовлетворяет области определения функции");
+            else
+                Console.WriteLine($"f({argument}) = {function1(argument)}");
+            Console.ReadKey();
+        }
+        static double function1(double x)
+        {
+          return (Math.Sqrt(x + 1) + Math.Sqrt(x - 1))/Math.Sqrt(x);
+            
+        }
+    }
+}
