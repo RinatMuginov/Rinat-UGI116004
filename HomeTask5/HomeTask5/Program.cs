d86d5cccf5d9c0f1894d1c6d44d9376bcd441725
Rinat.Muginov <Rinat.Muginov@urfu.me>
2021-10-26 02:08:40 +0500

    задание 5.2 выполнено

---
+++ HomeTask5/HomeTask5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeTask5
+{
+    class Program
+    {
+        static void Main()
+        {
+            string StringKosmos, StringSmeh;
+            string FirstString = "микросхема";
+            Console.WriteLine("Слова \"космос\" и \"смех\", полученные из слова \"микросхема\": ");
+            StringKosmos = FirstString.Substring(2,1)+ FirstString.Substring(4, 2) + FirstString.Substring(0, 1) + FirstString.Substring(4, 2);
+            Console.WriteLine(StringKosmos);
+            StringSmeh = FirstString.Substring(5, 1) + FirstString.Substring(8, 1) + FirstString.Substring(7, 1) + FirstString.Substring(6, 1);
+            Console.WriteLine(StringSmeh);
+
+            Console.ReadKey();
+        }
+         
+    }   
+}
