b098b7583df06a946b5f9e920bf4b6b5e16824ff
Rinat.Muginov <Rinat.Muginov@urfu.me>
2021-10-26 00:14:55 +0500

    вариант 16, домашнее задание 4

---
+++ HomeTask4/HomeTask4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeTask4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"Значение x для варианта 16 равно {Method16(2,3) + Method16(3,7) + Method16(5,5)}");
+        }
+        static double Method16(double x, double y)
+        {
+            return Math.Sqrt(x + Math.Log(1 + Math.Sqrt(y)));
+        }
+    }
+}
