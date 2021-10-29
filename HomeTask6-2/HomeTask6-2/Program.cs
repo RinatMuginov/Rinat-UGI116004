5d8e587ff142c341d3cce46646e6ea17a279d7ae
Rinat.Muginov <Rinat.Muginov@urfu.me>
2021-10-29 05:13:15 +0500

    выполнено задание 6.2

---
+++ HomeTask6-2/HomeTask6-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeTask6_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите координаты для того, чтобы проверить принадлежит ли точка области варианта 16");
+            var CoordinateX = double.Parse(Console.ReadLine());
+            var CoordinateY = double.Parse(Console.ReadLine());
+            if ((XandYonGraphic(CoordinateX, CoordinateY)) == true)
+                Console.WriteLine($"Точка ({CoordinateX};{CoordinateY}) принадлежит заданной области");
+            else
+                Console.WriteLine($"Точка ({CoordinateX};{CoordinateY}) не принадлежит заданной области");
+
+        }
+        static bool XandYonGraphic (double x,double y)
+        {
+            if ((x <= 1.5) && (x >= -1) && (y <= 2) && (y >= -0.5))
+                return true;
+            else
+                return false;
+        }
+    }
+}
