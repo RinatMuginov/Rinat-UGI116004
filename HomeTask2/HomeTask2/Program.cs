9435a976fd641c4e7b7c8a49b3b57b15d64bb659
Rinat.Muginov <Rinat.Muginov@urfu.me>
2021-11-04 02:21:38 +0500

    выполнено задание 2

---
+++ HomeTask2/HomeTask2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeTask2
+{
+    class Program
+    {
+        static void Main()
+        {
+            double volume, square;
+            Console.WriteLine("Введите значение длины стороны основания правильной треугольной пирамиды");
+            var side = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение высоты правильной треугольной пирамиды");
+            var height = double.Parse(Console.ReadLine());
+            volume = (height * Math.Sqrt(3) * side * side * 0.25) / 3;
+            Console.WriteLine($"Объем правильной треугольной пирамиды равен {volume}");
+            square = (0.5 * (side + side + side) * Math.Sqrt(height * height + ((side * side * 3) / 36))) + side * side * Math.Sqrt(3) * 0.25;
+            Console.WriteLine($"Площадь поверхности правильной треугольной пирамиды равна {square}");
+        }
+    }
+}
---
+++ HomeTask6.3/HomeTask6.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeTask6._3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
