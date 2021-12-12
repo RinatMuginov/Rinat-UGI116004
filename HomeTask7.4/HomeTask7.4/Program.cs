3ca39fda8991a1d05584a2fe2604fdb80dc2b62d
Rinat.Muginov <Rinat.Muginov@urfu.me>
2021-12-13 02:39:55 +0500

    выполнено задание 7.4

---
+++ HomeTask7.4/HomeTask7.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeTask7._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, для которого нужно найти количество значащих нулей в двоичной записи");
+            var n = int.Parse(Console.ReadLine());
+            int k = 0;
+            for (int i = 1; ; i++)
+            {
+                if (n % 2 == 0)
+                    k++;
+                n = n / 2;
+                if (n == 0)
+                    break;
+            }
+            Console.WriteLine($"Нулей в двоичной записей числа: {k}");
+        }
+    }
+}
