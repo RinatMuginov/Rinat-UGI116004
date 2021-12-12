bb5e3bf5f75a56bd154b2984a3a1546e9c47a76c
Rinat.Muginov <Rinat.Muginov@urfu.me>
2021-12-13 02:35:12 +0500

    выполнено задание 7.3

---
+++ HomeTask7.3/HomeTask7.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeTask7._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите огранчиение E для задания 3 в варинте 16");
+            double E = double.Parse(Console.ReadLine());
+            double a = 1;
+            for (int i = 1; ; i++)
+            {
+                if (Math.Abs(2 - a) < E)
+                {
+                    Console.WriteLine($"Для ограничения {E} подходит значение {a} под индексом {i}");
+                    break;
+                }
+                else
+                    a = a + Math.Pow(0.5, i);
+            }
+        }
+    }
+}
