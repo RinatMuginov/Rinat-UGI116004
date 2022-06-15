12415148658fae721d3559b902a40ba4527e01a4
Rinat.Muginov <Rinat.Muginov@urfu.me>
2022-06-16 04:42:14 +0500

    сделано

---
+++ homework11/OrderTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using homework11;
+using NUnit.Framework;
+
+namespace OrderTest
+{ 
+    [TestFixture]
+    class OrderTest
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var soap = CreateTestOrder();
+
+            Assert.AreEqual("Soap", soap.ProductName);
+            Assert.AreEqual(14042003, soap.VendorCode);
+            Assert.AreEqual("Ivanov", soap.СourierSurname);
+            Assert.AreEqual(67, soap.RequestNumber);
+            Assert.AreEqual("14.06.2022", soap.DeliveryTime);
+            Assert.AreEqual(DType.Express, soap.OrderType);
+    }
+        [Test]
+        public void ToString_Order_ProductNameSpaceCourierSurname()
+        {
+            var soap = CreateTestOrder();
+
+            Assert.AreEqual("Soap Ivanov", soap.ToString());
+        }
+        private Order CreateTestOrder()
+        {
+            return new Order ("Soap", 14042003, "Ivanov", 67, "14.06.2022", DType.Express);
+        }
+    }
+}
---
+++ homework11/homework11/Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace homework11
+{
+    public class Order
+    {
+        public string ProductName;
+        public readonly int VendorCode;
+        public string СourierSurname;
+        public readonly int RequestNumber;
+        public DateTime DeliveryTime;
+        public DType OrderType;
+        public Order(string productname, int vendorcode, string couriersurname, int requestnumber, string deliverytime, DType ordertype)
+        {
+            ProductName = productname;
+            VendorCode = vendorcode;
+            СourierSurname = couriersurname;
+            OrderType = ordertype;
+        }
+        public override string ToString()
+        {
+            return $" Имя товара:{ProductName}. Артикул: {VendorCode}.";
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine(this);
+
+            var ordertype = "";
+            switch (OrderType)
+            {
+                case DType.Regular:
+                    ordertype = "обычный";
+                    break;
+                case DType.Express:
+                    ordertype = "срочный";
+                    break;
+            }
+            Console.WriteLine($"Фамилия курьера: {СourierSurname}. Номер заявки: {RequestNumber}. Дата доставки: {DeliveryTime:d}. Тип заказа: {ordertype}.");
+        }
+
+        }
+    }
+
