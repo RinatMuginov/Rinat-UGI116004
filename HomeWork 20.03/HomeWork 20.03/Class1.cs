7c37f458e5b6ac0af47e5b2cad7d30f6cf7b4e56
Rinat.Muginov <Rinat.Muginov@urfu.me>
2022-06-16 04:32:09 +0500

    Сделано

---
+++ HomeWork 20.03/HomeWork 20.03/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_20._03
+{
+    public class Person
+    {
+        public string Name;
+        public string Surname;
+        public readonly DateTime Birthday;
+        public readonly PersonGender Gender;
