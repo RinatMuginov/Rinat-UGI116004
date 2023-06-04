26916371c052a2f6241d7d02585d747f01220a64
Rinat.Muginov <Rinat.Muginov@urfu.me>
2023-06-04 14:32:35 +0500

    выполнено задание 5

---
+++ task5/Client/Record.cs
@@ -0,0 +1,18 @@
+﻿namespace ClientLibrary
+{
+    public struct Record
+    {
+        public int ClientID; 
+        public int Year; 
+        public int Month; 
+        public int Duration;
+
+        public Record(int clientID, int year, int month, int duration) : this() 
+        { 
+            ClientID = clientID;   
+            Year = year;    
+            Month = month;
+            Duration = duration;
+        }
+    }
+}
